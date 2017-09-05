be59f1e785a78d20264107ad6d98b9db8a4c01c8
Gamiz <ldgamiz12@gmail.com>
2017-09-05 11:55:05 -0500

    Añadir menu

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es_MX">
+<head>
+	<meta charset="UTF-8">
+	<title>SE & SE</title>
+
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	
+	<link href="css/style.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<div class="menu-bar">
+				<span class="fa fa-bars"> Menú</span>
+			</div>
+		<div class="menu">
+			<div class="links">
+				<a href="#">Inicio</a>
+				<a href="#">Acerca de</a>
+				<a href="#">Equipo</a>
+				<a href="#">Ubicación</a>
+				<a href="#">Contacto</a>
+			</div>
+		</div>
+	</header>
+	
+	<script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="js/menu.js"></script>
+</body>
+</html>
