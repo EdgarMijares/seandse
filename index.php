13dc0aa3634932c509cd4839253a0d764f49b637
Edgar <progresssystemsoftware@gmail.com>
2017-09-12 14:49:14 -0500

    Full page

--- index.php
+++ index.php
@@ -11,43 +11,32 @@
 </head>
 <body>
 	<header>
-		<div class="menu-bar">
-				<span class="fa fa-bars"> Menú</span>
-			</div>
-		<div class="menu">
-			<div class="links">
-				<a href="#">Inicio</a>
-				<a href="#">Acerca de</a>
-				<a href="#">Equipo</a>
-				<a href="#">Ubicación</a>
-				<a href="#">Contacto</a>
-			</div>
-		</div>
-		<div class="banner">
-			<img src="img/logo.svg" alt="Logo" class="logo">
-		</div>
+		<?php include("menu.html") ?>
 	</header>
-
-	<div class="acerca">
-		<h2>Acerca de</h2>
-		
-		<div class="shape"></div>
-		<div class="mision">
-			<p>
-				Misión: <br>
-				Ser una empresa especialista de servicios de fletes, 
-				mudanzas y Transportación de bienes muebles y 
-				mercancías, brindándoles un servicio de alta 
-				calidad logística en el cuidado de bienes y
-				productos conun destino seguro, con un 
-				sello de honestidad y espíritu de 
-				servicio.
-			</p>
-		</div>
-		<div class="vision">
+	
+	<body>
+		<div class="acerca">
+			<h2>Acerca de</h2>
 			
+			<div class="shape"></div>
+			<div class="mision">
+				<p>
+					Misión: <br>
+					Ser una empresa especialista de servicios de fletes, 
+					mudanzas y Transportación de bienes muebles y 
+					mercancías, brindándoles un servicio de alta 
+					calidad logística en el cuidado de bienes y
+					productos conun destino seguro, con un 
+					sello de honestidad y espíritu de 
+					servicio.
+				</p>
+			</div>
+			<div class="vision">
+				
+			</div>
+		
 		</div>
-	</div>
+	</body>
 	
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<script src="js/menu.js"></script>
