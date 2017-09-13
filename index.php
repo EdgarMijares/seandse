479300655d487e1badfc1482fb4f90d13976a6be
Gamiz <ldgamiz12@gmail.com>
2017-09-13 09:32:31 -0500

    Añadir secciones

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	
-	<link rel="stylesheet" type="text/css" href="css/normalize.min">
+	<link rel="stylesheet" type="text/css" href="css/normalize.min.css">
 	<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 	
 	<link href="css/style.css" type="text/css" rel="stylesheet">
@@ -18,33 +18,19 @@
 	</header>
 	
 	<body>
-		<div class="acerca">
-			<h2>Acerca de</h2>
-			
-			<div class="shape"></div>
-			<div class="mision">
-				<p>
-					Misión: <br>
-					Ser una empresa especialista de servicios de fletes, 
-					mudanzas y Transportación de bienes muebles y 
-					mercancías, brindándoles un servicio de alta 
-					calidad logística en el cuidado de bienes y
-					productos conun destino seguro, con un 
-					sello de honestidad y espíritu de 
-					servicio.
-				</p>
-			</div>
-			<div class="vision">
-				
-			</div>
-		
+		<div id="fullpage">
+			<div class="section">Seccion 1</div>
+			<div class="section">Seccion 2</div>
+			<div class="section fp-auto-height">Seccion 3</div>
+			<div class="section">Seccion 4</div>
+			<div class="section">Seccion 5</div>
 		</div>
 	</body>
 	
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script src="vendors/jquery.easings.min.js"></script>
-	<script type="text/javascript" src="vendors/scrolloverflow.min.js"></script>
-	<script type="text/javascript" src="jquery.fullPage.js"></script>
+	<script src="js/vendors/jquery.easings.min.js"></script>
+	<script type="text/javascript" src="js/vendors/scrolloverflow.min.js"></script>
+	<script type="text/javascript" src="js/jquery.fullPage.js"></script>
 	<script src="js/menu.js"></script>
 </body>
 </html>
--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 $(document).ready(main);
+$(document).ready(fullpag);
  
 var contador = 1;
 var alto;
@@ -18,10 +19,21 @@ function main(){
 		}
 	});
 	center();
+	
 };
 
 function center() {
 	alto = ($(window).height()/2) - $('.logo').height()/2;
 	console.log("El alto es de" + alto);
 	$('.logo').css("top",alto);
+
+	alto2 = $(".banner").height() + 100
+	console.log("El alto2 es de" + alto2);
+	//$('#fullpage').css("top", alto2);
+}
+
+function fullpag() {
+	$('#fullpage').fullpage({
+		 sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+	});
 }
