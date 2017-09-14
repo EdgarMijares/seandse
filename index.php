821ec7868562ee125d081ec808a0db00724120b2
Edgar <progresssystemsoftware@gmail.com>
2017-09-14 08:24:18 -0500

    Agregar grids

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	
-	<link rel="stylesheet" type="text/css" href="css/normalize.min.css">
+	<link rel="stylesheet" href="css/sass/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
 	
 	<link href="css/style.css" type="text/css" rel="stylesheet">
@@ -25,18 +25,43 @@
 				</div>
 			</div>
 			<div class="pantalla-dos section ">
-				
+				<div class="container">
+					<div class="row">
+						<div class="col s12 l6">
+							<h3>Misión:</h3>
+							<p> 
+								Ser una empresa especialista de servicios de fletes, mudanzas y Transportación de bienes muebles y mercancías, brindándoles un servicio de alta 
+								calidad logística en el cuidado de bienes y productos conun destino seguro, con un sello de honestidad y espíritu de servicio.
+							</p>
+						</div>
+						<br>
+						<div class="col s12 l5 offset-l7 secon">
+							<h3>Visión:</h3>
+							<p>Somos una empresa líder Mexicana de logística en Fletes, Mudanzas y Transportación de bienes Muebles y mercancías.</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col l12 offset-m5">
+							<img src="img/logo.svg" alt="Logo" class="logo">
+						</div>
+					</div>
+				</div>
 			</div>
-			<div class="pantalla-tres section">Seccion 3</div>
+			<div class="pantalla-tres section">
+				<div class="container">
+					
+				</div>
+			</div>
 			<div class="pantalla-cuatro section">Seccion 4</div>
 			<div class="pantalla-cinco section">Seccion 5</div>
 		</div>
 	</body>
 
-	<script src="http://code.jquery.com/jquery-latest.js"></script>
-	<script src="js/vendors/jquery.easings.min.js"></script>
-	<script type="text/javascript" src="js/vendors/scrolloverflow.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="js/vendor/jquery.easings.min.js"></script>
+	<script type="text/javascript" src="js/vendor/scrolloverflow.min.js"></script>
 	<script type="text/javascript" src="js/jquery.fullPage.js"></script>
-	<script src="js/menu.js"></script>
+	<script type="text/javascript" src="js/menu.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
